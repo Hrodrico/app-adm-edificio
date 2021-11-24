919414be3868c5ca57d7e8f787c062cdcd7154fa
Rodrigo Palma Raggi <ext_randpalmar@falabella.cl>
2021-11-24 19:03:11 -0300

    feat: change name business

--- adm-edificio-palma-raggi/src/App.js
+++ adm-edificio-palma-raggi/src/App.js
@@ -7,35 +7,37 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ProductCard from './components/ProductCard/ProductCard';
 
+//Img
+import logo from './assets/images/magia_di_victoria.png'
+import honey_mask from './assets/images/p_honey_mask.jpeg'
+import hand_spray from './assets/images/p_antibacterial_hand_spray.jpeg'
+import billetera from './assets/images/p_billetera.jpeg'
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<HeaderFloating/>				
+				<HeaderFloating imgBusiness={logo} nameBusiness="La Magia Di Victoria"/>				
 				<NavBar /> 
 				<ItemListContainer greeting="Bienvenido a Min-Deptos!!!"/>
 				<div className='UserSection'>
 					<ProductCard
-						name='Santa Clara / Huechuraba'
-						date='Publicado hace 2 meses'
-						description='Acogedora casa de 2 pisos, consta de living comedor, cocina, acceso para vehículo techado, orientada al Sur. 83m2/122m2 aprox. Excelente ubicación, cercano a comercio variado, áreas verdes y a estación de metro Vespucio Norte. Santa Clara / Huechuraba'
-						img='https://http2.mlstatic.com/D_NQ_NP_994448-MLC47634098749_092021-O.webp'
+						name='Antibacterial Hand Spray'
+						date='24-Nov-2021'
+						description='Spray antibacterial'
+						img={hand_spray}
 					/>
 					<ProductCard
-						name='Edificio Pirineos'
-						date='Proyecto - Entrega 3 Trimestre 2021'
-						description='Edificio Pirineos de Inmobiliaria Inspira es un edificio que combina perfectamente la esencia de la comuna de Providencia, usando elementos arquitectónicos de los años 50 con un estilo moderno que rompe el concepto tradicional.
-
-						Está ubicado en un sector privilegiado de Providencia. A una cuadra de Pedro de Valdivia, a 3 cuadras de Av. Francisco Bilbao y de la estación de Metro Inés de Suárez. Cercano a comercios, colegios, supermercados, restaurantes, parques, plazas, bancos y todo lo que necesites.'
-						img='https://http2.mlstatic.com/D_NQ_NP_850858-MLC48196018761_112021-O.webp'
+						name='Honey Mask'
+						date='24-Nov-2021'
+						description='Mascarilla, limpia y exfolia, todo en uno. Incluye menta, cáscara de limón y miel para nutrir y suavizar en un solo paso refrescante. Usar en la cara y el cuerpo para un tratamiento de pies a cabeza.'
+						img={honey_mask}
 					/>
 					<ProductCard
-						name='Casa Condominio La Toscana, Huechuraba'
-						date='Publicado hace 24 días'
-						description='Oficina de propiedades ofrece hermosa casa en condominio ubicada en la comuna de Huechuraba, cuenta con seguridad las 24 horas, áreas verdes, sector tranquilo, amplias calles de acceso, cercana a colegios, supermercados, mall y autopistas.
-						Tramitamos crédito hipotecario en diferentes bancos, obteniendo la mejor opción para su compra.'
-						img='https://http2.mlstatic.com/D_NQ_NP_854005-MLC47792570749_102021-O.webp'
+						name='Billetera Victoria Secret`s'
+						date='24-Nov-2021'
+						description='Billeteras Victoria Secret`s'
+						img={billetera}
 					/>					
 				</div>
 				
--- adm-edificio-palma-raggi/src/components/Header/Header.js
+++ adm-edificio-palma-raggi/src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Header, Segment, Icon } from 'semantic-ui-react'
+import { Header, Segment, Icon, Image } from 'semantic-ui-react'
 
-const HeaderFloating = () => (
+const HeaderFloating = ({imgBusiness, nameBusiness}) => (
   <Segment clearing>
-    {/* <Header as='h4' floated='left'>Bienvenido a Min-Deptos!!!</Header> */}
+    <Header as='h4' floated='left'><Image className="logo" src={imgBusiness} />{nameBusiness}</Header>
     <Header as='h4' floated='right'><Icon name='user'/>Log in</Header>
   </Segment>
 )
