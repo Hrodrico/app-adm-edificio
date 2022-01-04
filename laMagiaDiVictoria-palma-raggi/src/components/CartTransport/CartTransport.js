4a1eac1872cdae639c9782ee7d991743dc3176f8
Rodrigo Palma Raggi <ext_randpalmar@falabella.cl>
2022-01-04 03:48:02 -0300

    feat: validacion formulario formik and yup

--- laMagiaDiVictoria-palma-raggi/src/components/CartTransport/CartTransport.js
+++ laMagiaDiVictoria-palma-raggi/src/components/CartTransport/CartTransport.js
@@ -2,45 +2,47 @@ import React, { useState, useContext } from "react";
 import { Divider, Form, Label, Button, Header, Icon } from 'semantic-ui-react'
 import Swal from 'sweetalert2'
 import { CartContext } from 'context/CartContext';
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 //Firebase
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "firebase/FirebaseConfig"
 
 function CartTransport() {
-    const objClient = {
-        name: '',
-        lastName: '',
-        email: '',
-        address: ''
-    };
-
-    const [client, setClient] = useState(objClient);
+    const objClient = { name: '', lastName: '', email: '', repeatEmail: '', address: '' };
     const { itemCard, itemClear, setterStep } = useContext(CartContext);
     const [buyId, setBuyId] = useState('');
-    const [buyedProducts, setBuyedProducts] = useState('');
+    const [buyedProducts, setBuyedProducts] = useState({...itemCard});
 
+    const Formik = useFormik({
+        initialValues: objClient,
+        validationSchema: Yup.object({
+            name: Yup.string().required("El nombre es obligatorio"),
+            lastName: Yup.string().required("El apellido es obligatorio"),
+            email: Yup.string().email("No es un e-mail valido").required("El e-mail es obligatorio"),
+            repeatEmail: Yup.string().email("No es un e-mail valido").required("El reingreso e-mail es obligatorio").oneOf([Yup.ref("email")],"E-mail no son iguales "),
+            address: Yup.string().required("La dirección es obligatorio"),
+        }),
+        onSubmit:(formData)=>{
+            console.log("formData::",formData);
+            getFinishedPurchase(formData);
+        }
+    });
     
     /* Price total of Carro */
     const priceTotal =  itemCard.reduce((totalCart, item) => totalCart + (item.price * item.quantity), 0);
-
-    const onChange = (e) => {
-		const { name, value } = e.target;
-		setClient({...client, [name]: value })
-		setBuyedProducts({...itemCard})
-	};
 
     const getReviewOrder = () => {
         setterStep(1);
     }
 
     /* Msge finished buy */
-    const getFinalizar = async (e) => {
+    const getFinishedPurchase = async (data) => {
         setterStep(4);
-        e.preventDefault()
         const saleDate = new Date().toLocaleDateString();
-        const docRef = await addDoc(collection(db, "purchaseOrder"), {client, buyedProducts, priceTotal, saleDate})
+        const docRef = await addDoc(collection(db, "purchaseOrder"), {data, buyedProducts, priceTotal, saleDate})
         setBuyId(docRef)
-        setClient(objClient)
         console.log("docRef::",docRef.id );
         console.log("buyId::",buyId);
         Swal.fire({
@@ -48,11 +50,11 @@ function CartTransport() {
             title: '<b>Gracias por su compra!!!</b>',
             html:'Su código de compra es: [' + docRef.id + '] <br>Te esperamos pronto!!!',
         })
-        //Limpia  carro
+        //Limpia  carro 15seg.
         setTimeout(() => {
             itemClear();
             setterStep(1);
-        }, 10000);
+        }, 15000);
     }
 
     return (
@@ -60,29 +62,35 @@ function CartTransport() {
             <Divider horizontal>
                     <Header as='h4'><Icon name='plane' />Revisión del Transporte</Header>
             </Divider>
-             <Form>
+            <Form onSubmit={Formik.handleSubmit}>
                 <Form.Field inline>
-                    <input type='text' placeholder='Nombre' name='name' onChange={onChange} value={client.name} />
-                    <Label pointing='right'>Favor, ingrese Nombre</Label>
+                    <Form.Input type='text' placeholder='Nombre' name='name' onChange={Formik.handleChange}  value={Formik.values.name} error={Formik.errors.name} />
+                    <Label pointing>Favor, ingrese Nombre</Label>
                 </Form.Field>
                 <Divider />
                 <Form.Field inline>
-                    <input type='text' placeholder='Apellido' name='lastName' onChange={onChange}  value={client.lastName} />
-                    <Label pointing='right'>Favor, ingrese Apellido</Label>
+                    <Form.Input type='text' placeholder='Apellido' name='lastName'  onChange={Formik.handleChange}  value={Formik.values.lastName}  error={Formik.errors.lastName} />
+                    <Label pointing>Favor, ingrese Apellido</Label>
+                    {/* <Label pointing='right'>Favor, ingrese Apellido</Label> */}
                 </Form.Field>
                 <Divider />
                 <Form.Field>
-                    <input type='text' placeholder='E-mail' name='email' onChange={onChange} value={client.email} />
+                    <Form.Input type='text' placeholder='E-mail' name='email' onChange={Formik.handleChange}  value={Formik.values.email} error={Formik.errors.email} />
                     <Label pointing>Favor, ingrese E-mail</Label>
                 </Form.Field>
                 <Divider />
                 <Form.Field>
-                    <input type='text' placeholder='Dirección' name='address' onChange={onChange}  value={client.address} />
+                    <Form.Input type='text' placeholder='Reingrese E-mail' name='repeatEmail'  onChange={Formik.handleChange}  value={Formik.values.repeatEmail} error={Formik.errors.repeatEmail} />
+                    <Label pointing>Favor, reingrese E-mail</Label>
+                </Form.Field>
+                <Divider />
+                <Form.Field>
+                    <Form.Input type='text' placeholder='Dirección' name='address' onChange={Formik.handleChange}   value={Formik.values.address} error={Formik.errors.address} />
                     <Label pointing>Favor, ingrese Dirección</Label>
                 </Form.Field>
                 <Button content='Volver' secondary onClick={ getReviewOrder} />
-				<Button content='Limpiar' secondary onClick={() => setClient(objClient)} />
-                <Button content='Finalizar Compra' primary onClick={ getFinalizar } type="submit"/>
+				<Button content='Limpiar' secondary onClick={ Formik.handleReset } />
+                <Button content='Finalizar Compra' primary type="submit"/>
             </Form>
         </div>
     )
--- laMagiaDiVictoria-palma-raggi/src/index.js
+++ laMagiaDiVictoria-palma-raggi/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 // SEMANTIC UI
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
 
